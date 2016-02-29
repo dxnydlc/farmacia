442e8d6b6918ab5b68bafd524b6691226df12c2a
Dxny boy <dany@delacruz.pe>
2016-02-28 23:47:44 -0500

    Login

---
+++ app/Http/Controllers/logController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace farmacia\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+use Session;
+use Redirect;
+
+use farmacia\Http\Requests;
+use farmacia\Http\Requests\loginRequest;
+use farmacia\Http\Controllers\Controller;
+
+class logController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('login.form');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if( Auth::attempt( ['email' => $request['email'] , 'password' => $request['password'] ]  ) ){
+            return "Logeado";
+        }
+        return "Error";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,10 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-
+Route::resource('login','logController');
 
 /*
 |--------------------------------------------------------------------------
